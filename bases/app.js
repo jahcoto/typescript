6e81e2dc9b02a08ceb19226c52ab7de11787ab4e
jahcoto <jahcoto@gmail.com>
2022-11-28 18:07:07 -0600

    Inicio de bases de typescript

---
+++ bases/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const msg = 'Hola';
+const heroe = {
+    name: 'Ironman',
+    age: 45,
+};
+heroe.age = 50;
+console.log(heroe.age + 1);
---
+++ bases/app.ts
@@ -0,0 +1,9 @@
+const msg: string = 'Hola';
+
+const heroe = {
+  name: 'Ironman',
+  age: 45,
+};
+
+heroe.age = 50;
+console.log(heroe.age + 1);
